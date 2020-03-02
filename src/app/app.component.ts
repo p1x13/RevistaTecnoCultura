c5c87ccfc88b9a228d85863ba38bc4139cba69ae
Gabriel Armando Perez Ramos <sergedxd@gmail.com>
2020-03-02 14:40:09 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RevistaTecnoCultura';
+  title = 'Revista_Tecno_Cultura';
 }
